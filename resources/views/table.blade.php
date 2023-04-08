1df684356c231e3af548bbe2cf68323503429e7c
fadlyian <mansyuraw24@gmail.com>
2023-04-08 16:33:53 +0700

    fixing task 4

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -19,7 +19,19 @@
                         </thead>
                         {{-- Task: add the loop here to show users, or the row "No content" --}}
                         <tbody>
-                            @if(count($users))
+                            @forelse ($users as $user)
+                            <tr>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>{{ $user->created_at }}</td>
+                            </tr>
+                            @empty
+                            <tr>
+                                <td colspan="3">No content.</td>
+                            </tr>
+                            @endforelse
+                            
+                            {{-- @if(count($users) > 0)
                                 @foreach ($users as $user)
                                     <tr>
                                         <td>{{ $user->name }}</td>
@@ -31,7 +43,7 @@
                                 <tr>
                                     <td colspan="3">No content.</td>
                                 </tr>
-                            @endif
+                            @endif --}}
                         </tbody>
                     </table>
                 </div>
